5ceb4f292b27e2233ad8fb752b69ccb7c0c912b8
Melissa Psaras <mrp227@cornell.edu>
2020-10-20 22:06:10 -0400

    further implementation of manual.ml

--- manual.ml
+++ manual.ml
@@ -13,15 +13,12 @@ type task = {
   created_date : string;
   due_date : string;
   priority : int;
-  category : 
-
-
 }
 
 (** The type representing a to-do list.  Type is [task list] *)
 type t = {
   name : string;
-  task_list : task list;
+  task_list : task list; 
 }
 
 (** Raised when an unknown task is encountered. *)
@@ -30,19 +27,39 @@ exception UnknownTask of task
 (** Raised when an invalid task is encountered. *)
 exception InvalidTask 
 
-(** Initialize an empty to-do list *)
-let empty_list
-val empty_list : unit
+(** Raised when a category is not found. *)
+exception CategoryNotFound of string
 
-(** [add_task t task] is the incompleted to-do list [t] with the new task [task]. *)
-val add_task : t -> task -> t
+
+let categories = ref []
+
+(** Initialize an empty to-do list *)
+let empty_list cat_name = {
+  name = cat_name;
+  task_list = [];
+}
+
+
+let add_task t task = {
+  name = t.name;
+  task_list = task :: t.task_list
+}
+
+
+let add_task_to_category cat_name task = 
+  try add_task (List.find (fun x -> x.name = cat_name) (!categories)) task
+  with Not_found -> begin
+      let new_cat = add_task (empty_list cat_name) task in
+      ignore (new_cat :: !categories); new_cat
+    end
 
 (** [complete_task t task] is a updated completed to-do list [t] with [task]. *)
-val complete_task : t -> task -> t
+(*val complete_task : t -> task -> t*)
+
 
 (** [delete_task t task] is an updated to-do list with [task] removed from [t] . *)
-val delete_task : t -> task -> t
+(*val delete_task : t -> task -> t
 
-
+  let new_cat *)
 
 
--- manual.mli
+++ manual.mli
@@ -18,17 +18,27 @@ exception UnknownTask of task
 (** Raised when an invalid task is encountered. *)
 exception InvalidTask 
 
-(** Initialize an empty to-do list *)
-val empty_list : unit
+(** Raised when a category is not found. *)
+exception CategoryNotFound of string
+
+(** [empty_list cat_name] initializes an empty to-do list with category name [cat_name]. *)
+val empty_list : string -> t
 
 (** [add_task t task] is the incompleted to-do list [t] with the new task [task]. *)
 val add_task : t -> task -> t
+
+(** [add_task_to_category cat_name task ] is the incompleted categorized to-do list [t] that has [cat_name] with the new task [task]. *)
+val add_task_to_category : string -> task -> t
 
 (** [complete_task t task] is a updated completed to-do list [t] with [task]. *)
 val complete_task : t -> task -> t
 
 (** [delete_task t task] is an updated to-do list with [task] removed from [t] . *)
 val delete_task : t -> task -> t
+
+
+
+
 
 
 
