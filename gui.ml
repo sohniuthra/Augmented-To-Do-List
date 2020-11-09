1877f80bab501f9c061cba6eb40ac7a1f9e634b8
Jyne Dunbar <jcd322@cornell.edu>
2020-11-09 17:03:10 -0500

    some user interaction

--- gui.ml
+++ gui.ml
@@ -1,7 +1,22 @@
 open Graphics
 open Manual
+open Printf
 
-let rec loop () = loop ()
+let rec loop () = 
+  let e = wait_next_event [Mouse_motion; Key_pressed] in
+
+  (*let mouse_description = sprintf "Mouse position: %d,%d" e.mouse_x e.mouse_y in*)
+  let key_description = if e.keypressed then sprintf "Key %c was pressed" e.key 
+    else "" in
+
+  let n_pressed = if e.key = 'n' then sprintf "new task" else "" in
+
+  clear_graph ();
+  moveto 0 100; draw_string key_description;
+  moveto 0 200; draw_string n_pressed;
+  (*moveto 0 0; draw_string mouse_description;*)
+
+  if e.key <> 'q' then loop () else ()
 
 let open_window = open_graph " 640x480"; set_window_title "To-Do List"
 
@@ -41,5 +56,6 @@ let () = open_window;
   set_color black;
   moveto 10 420;
   draw_str_list ["a"; "cat"];
-  loop ()
+  loop ();
+  close_graph ();
 
