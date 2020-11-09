adb2d7d304d54846496e8bf53f1d77c509cc9ca1
Jyne Dunbar <jcd322@cornell.edu>
2020-11-09 01:14:07 -0500

    few more gui functions

--- gui.ml
+++ gui.ml
@@ -16,19 +16,28 @@ let draw_task (tsk : Manual.task) =
   draw_string "" (*(string_of_int tsk.priority)*)
 
 
-(** [draw_tsk_list t] takes task list [t] and draws it with each item on a new 
-    line *) 
+(** [draw_tsk_list tlst] takes task list [tlst] and draws it with each item on 
+    a new line *) 
 let rec draw_tsk_list tlst =
   match tlst with 
   | [] -> ()
   | h::t -> draw_task h; moveto (current_x ()) (current_y () - 15); 
     draw_tsk_list t
 
+(** [draw_str_list slst] takes string list [slst] and draws it with each item 
+    on a new line *) 
+let rec draw_str_list slst =
+  match slst with 
+  | [] -> ()
+  | h::t -> draw_string h; moveto (current_x ()) (current_y () - 15); 
+    draw_str_list t
+
 let () = open_window;
   moveto 10 460;
   set_color blue;
-  draw_string "Welcome to your new to-do list"; 
+  draw_string "Welcome to your new to-do list!"; 
   moveto 10 440;
+  draw_string "Press 'n' to create a new task";
   set_color black;
   loop ()
 
