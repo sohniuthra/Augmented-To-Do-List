17a6bc4c31e48e2e015c2f81152cbfb962d9c9d0
Pooja Kanumalla <pk457@cornell.edu>
2020-11-10 13:06:53 -0500

    added tests

---
+++ _build/author.mli
@@ -0,0 +1,39 @@
+(************************************************************ 
+   Copyright (C) 2020 Cornell University.
+   Created by Sohni Uthra (su58@cornell.edu), Melissa Psaras 
+   (mrp227@cornell.edu), Pooja Kanumalla (pk457@cornell.edu), and Jyne Dunbar 
+   (jcd322@cornell.edu). You may not redistribute this project, distribute any 
+   derivatives, or use it for commercial purposes.
+ ************************************************************) 
+
+(** CS 3110 Fall 2020 Software Engineering Final Project
+    @author Sohni Uthra (su58)
+    @author Melissa Psaras (mrp227) 
+    @author Pooja Kanumalla (pk457)
+    @author Jyne Dunbar (jcd322)*)
+
+(************************************************************ 
+
+   Academic Integrity Statement
+
+   We, the people named in the @author comments above, have fully reviewed the
+   course academic integrity policies.  We have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If we did engage in such collaboration, here is the
+   list of other students with whom we collaborated, and a brief summary of that
+   collaboration:
+
+   - none
+
+   Here are any deviations from the policies that we want to document, with the
+   understanding that being honest about our mistakes is more honorable than
+   lying about our intellectual work:
+
+   - none
+
+ ************************************************************)  
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
---
+++ _build/manual.mli
@@ -0,0 +1,82 @@
+(** 
+   Representation of a manual to-do list.
+
+   This module represents the data stored in a to-do list, including
+   data for each task.
+*)
+
+
+(** The abstract type representing a to-do list. *)
+type t 
+
+(** The abstract type representing one task of the to-do list *)
+type task
+
+(** Raised when an unknown task is encountered. *)
+exception UnknownTask of task
+
+(** Raised when an invalid task is encountered. *)
+exception InvalidTask 
+
+(** Raised when a category is not found. *)
+exception CategoryNotFound of string
+
+(* SPEC??? *)
+val access_cat : ?cat:(t list ref) -> unit -> t list
+
+(** [empty_cat ()] initializes an empty category list. *)
+val empty_cat : unit -> t list ref
+
+val todays_date : unit -> string
+
+(** [init_task name due_date priority] initializes a task with 
+    name [name], created date [created_date], due date [due_date], and priority 
+    [priority] *)
+val init_task : string -> string -> int -> task
+
+(** [init_todolist name task_list] initializes a to-do list with name [name] 
+    and task list [lst]. *)
+val init_todolist : string -> task list -> t
+
+(** [empty_list cat_name] initializes an empty to-do list with category name 
+    [cat_name]. *)
+val empty_list : string -> t
+
+(** [sort_list cat_name] sorts a to-do list with category [name] by priority
+    of tasks  *)
+val sort_list : ?cat:(t list ref) -> string -> unit
+
+(** [sort_task task cat_name] inserts a task into a sorted list - needed? *)
+
+(** [create_task cat_name name due_date priority] updates the to-do
+    list with name [cat_name] with the new task [name due_date priority]. 
+    If category name [cat_name] does not already exist, a new category with 
+    [cat_name] is created with task [name created_date due_date priority] 
+    in the task list. *)
+val create_task : ?cat:(t list ref) -> string -> string -> string -> int -> unit 
+
+val empty_cat : unit -> t list ref
+
+(** [complete_task t task] is an updated completed to-do list with name
+    [cat_name] with task named [task_name]. *)
+val complete_task : ?cat:(t list ref) -> string -> string -> unit
+
+(** [delete_task t task] is an updated to-do list with task with name 
+    [task_name ] deleted from category with name [cat_name]. *)
+val delete_task : ?cat:(t list ref) -> string -> string -> unit
+
+(** [to_list cat_name] is a list containing the same elements and the same 
+    category name [cat_name] as the category with name [cat_name].*)
+val to_list : ?cat:(t list ref) -> string ->  string list
+
+(** [to_list cat_name lst] is a list containing the same elements and the same 
+    category name [cat_name] as the category with name [cat_name] given 
+    a task list [lst] as an additional input.*)
+(* val to_list : string ->  task list -> string list *)
+
+
+
+
+
+
+
